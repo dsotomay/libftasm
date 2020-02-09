92289d16a3658d6a0c28630b0567c56e9612cd9a
Dylan Sotomayor <dylants365@gmail.com>
2020-02-08 23:22:12 -0800

    added defines and started setup

--- ft_cat.s
+++ ft_cat.s
@@ -3,8 +3,14 @@
 	global	_ft_cat
 	extern	_write
 	extern	_read
-%ifndef 
-%define
+%ifndef	BUF
+%define	BUF 1024
+%elif	BUF <= 0 || BUF >= 8388608
+%define	BUF 1024
 %endif
 section .text	align=16
 _ft_cat:
+	push	rbx
+	mov		ebx, edi
+	sub		rsp, BUF
+	
